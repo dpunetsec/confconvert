5c7c293dd5d9e3f4c09223032e864540952e1608
John Kristoff <jtk@p50.localdomain>
2016-08-04 14:48:24 -0500

    hacky conversion script nearly complete

--- ios2junos/edgeswitch.pl
+++ ios2junos/edgeswitch.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 
 use English;
+use Getopt::Std;
 use Net::Netmask;
 use Template;
 
@@ -11,40 +12,101 @@ no warnings 'experimental::smartmatch';
 
 $OUTPUT_AUTOFLUSH++;
 
-my %config;
+use constant USAGE =>
+"$0 [ -h ] [ -g gateway ] [ -l location ] [ -n hostname ]
+    [ -t template ] [ -v vlan_config ]
+    cisco_config_1 [ cisco_config_2 ] [ ... ]
+";
 
-# TODO: parse multiple IOS edge switch configs as a group into one JUNOS config
-#       switch number variable gets shoved it interface name
-my $switch = 0;
+my %config;
 
 # TODO: option to convert uplinks to ge or xe?
 
-while ( defined(my $line=<>) ) {
-    chomp $line;
+getopts( 'hg:l:n:t:v:', \my %opts );
+die USAGE if $opts{h};
+die USAGE if !@ARGV;
 
-    for($line) {
-        when    ( /^hostname (\S+)/ )               { $config{hostname} = $1 }
-        when    ( /^interface (\S+)/ )              { parse_interface($1) }
-        when    ( /^ip default-gateway (\S+)/ )     { $config{gateway} = $1 }
-        when    ( /^snmp-server location (\S+)/ )   { $config{location} = $1; }
-        default { next }
+$config{gateway}  = $opts{g} || '##GATEWAY##';
+$config{location} = $opts{l} || '##LOCATION##';
+$config{hostname} = $opts{n} || '##HOSTNAME##';
+
+my $template = $opts{t} || 'junos.tt';
+
+my @files = @ARGV;
+
+parse_vlanconf($opts{v});
+
+for my $i ( 0 .. $#files ) {
+    open ( my $CONFG, '<', $files[$i] ) or die USAGE;
+
+    while ( defined(my $line=<$CONFG>) ) {
+        chomp $line;
+        if ( $line =~ /^interface (\S+)/ ) {
+            parse_interface( { file => $CONFG, switch_num => $i, interface => $1 } );
+        }
     }
+
+    close $CONFG;
 }
 
-my $template = Template->new();
-
-# TODO: use an option to grab the template and output
-$template->process( 'junos.tt', \%config ) || die $template->error();
+my $tt = Template->new();
+$tt->process( $template, \%config ) || die $tt->error();
 
 exit 0;
 
+# TODO:
+## chassis/slot/port
+## for every slot==0 or slot/2 == 0, set ether-options for uplink port:
+##     ge-0/1/0 {
+##        ether-options {
+##            802.3ad ae0;
+##        }
+##    }
+
+sub parse_vlanconf {
+    my $vlanconf = shift or return;
+    my %vlan;
+
+    open ( my $VLANCONF, '<', $vlanconf ) or die "$vlanconf open error";
+
+    while( defined(my $line=<$VLANCONF>) ) {
+        chomp $line;
+
+        $line =~ s{ \s* \z }{}xms;  # remove trailing whitespace
+
+        next if $line =~ m{ \A \s* [#] }xms;  # comment lines
+        next if $line =~ m{ \A \s* \z }xms;   # blank lines
+        next if $line !~ m{ active \z }xms;   # only active interfaces
+
+        next if $line !~ m{ \A \s* (\d+) \s+ (\S+) }xms;
+
+        $config{vlans}{$1} = $2;  # vlan_number{vlan_name}
+    }
+
+    close $VLANCONF;
+
+    return;
+}
+
 sub parse_interface {
-    my $interface = ios2junos_ifname(shift) or return;
+    my ($arg_ref)  = @_;
+    my $file       = $arg_ref->{file} or return;
+    my $switch_num = $arg_ref->{switch_num};  # might be zero
+    my $interface  = $arg_ref->{interface} or return;
     my %ifconfig;
 
-    return if $interface eq 'vlan1';  # we will not convert any vlan1 interfaces
+    return if !defined $switch_num;  # zero is ok
 
-    while ( defined(my $line=<>) ) {
+    $interface = ios2junos_ifname(
+        { switch_num => $switch_num, interface => $interface }
+    ) or return;
+
+    # we will not convert any vlan1 interfaces
+    return if $interface eq 'vlan1';
+
+    while ( defined(my $line=<$file>) ) {
+        chomp $line;
+
         last if $line =~ /^[!]/;
 
         for($line) {
@@ -52,10 +114,11 @@ sub parse_interface {
                 $ifconfig{$interface}{description} = $1;
             }
             when ( /^ switchport access vlan (\d+)/ ) {
-                $ifconfig{$interface}{access_vlan} = $1;
+                $ifconfig{$interface}{access_vlan} = 'access-' . $config{vlans}{$1};
             }
             when ( /^ switchport voice vlan (\d+)/ ) {
-                $ifconfig{$interface}{voice_vlan} = $1;
+                # destination is not on the interface
+                $config{voice_vlan}{$1} = 1;
             }
             when ( /^ ip address (\S+) (\S+)/ ) {
                 $ifconfig{$interface}{address} = $1;
@@ -76,21 +139,24 @@ sub parse_interface {
 }
 
 sub ios2junos_ifname {
-    my $interface = shift or return;
-    # XXX: $switch is currently a global var, pass as arg?
+    my ($arg_ref)  = @_;
+    my $switch_num = $arg_ref->{switch_num};  # might be zero
+    my $interface  = $arg_ref->{interface} or return;
+
+    return if !defined $switch_num;  # zero is ok
 
     for ($interface) {
         when ( /^FastEthernet(\d+)\/(\d+)/ ) {
             my $type = 'ge';
             my $slot = $1;
             my $port = $2 - 1;
-            $interface = "$type-$switch/$slot/$port";
+            $interface = "$type-$switch_num/$slot/$port";
         }
         when ( /^GigabitEthernet(\d+)\/(\d+)/ ) {
             my $type = 'xe';
             my $slot = $1;
             my $port = $2 - 1;
-            $interface = "$type-$switch/$slot/$port";
+            $interface = "$type-$switch_num/$slot/$port";
         }
         default { $interface = lc $interface }
     }
